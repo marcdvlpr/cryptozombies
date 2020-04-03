10cfbde0ecf86701897c0fb1154859c2606d17c2
Marc Dos Santos <mdossantosdev@gmail.com>
2020-04-03 17:36:19 +0200

    Add approve function to ZombieOwnership contract

--- ZombieOwnership.sol
+++ ZombieOwnership.sol
@@ -5,6 +5,8 @@ import "./ERC721.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
 
+  mapping (uint => address) zombieApprovals;
+
   function balanceOf(address _owner) public view returns (uint256 _balance) {
     return ownerZombieCount[_owner];
   }
@@ -22,6 +24,11 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
 
   function transfer(address _to, uint256 _tokenId) public onlyOwnerOf(_tokenId) {
     _transfer(msg.sender, _to, _tokenId);
+  }
+
+  function approve(address _to, uint256 _tokenId) public onlyOwnerOf(_tokenId) {
+    zombieApprovals[_tokenId] = _to;
+    emit Approval(msg.sender, _to, _tokenId);
   }
 
 }
