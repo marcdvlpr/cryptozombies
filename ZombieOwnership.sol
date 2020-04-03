80cb518ffc5a2096e7a23437b4354a45e6b27509
Marc Dos Santos <mdossantosdev@gmail.com>
2020-04-03 14:42:46 +0200

    Add ERC721 contract and import to ZombieOwnership contract

--- ZombieOwnership.sol
+++ ZombieOwnership.sol
@@ -1,7 +1,9 @@
 pragma solidity ^0.4.19;
 
-import "./ZombieBattle.sol";
+import "./zombieattack.sol";
+import "./erc721.sol";
 
-contract ZombieOwnership is ZombieBattle {
+contract ZombieOwnership is ZombieAttack, ERC721 {
 
 }
+
