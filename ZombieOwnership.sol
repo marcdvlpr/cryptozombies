9a07e2e24fb3f0b20c06827c0d42d2ed77d05743
Marc Dos Santos <mdossantosdev@gmail.com>
2020-04-03 15:24:44 +0200

    Add balanceOf function to ZombieOwnership contract

--- ZombieOwnership.sol
+++ ZombieOwnership.sol
@@ -1,9 +1,12 @@
 pragma solidity ^0.4.19;
 
-import "./zombieattack.sol";
-import "./erc721.sol";
+import "./ZombieBattle.sol";
+import "./ERC721.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
 
-}
+  function balanceOf(address _owner) public view returns (uint256 _balance) {
+    return ownerZombieCount[_owner];
+  }
 
+}
